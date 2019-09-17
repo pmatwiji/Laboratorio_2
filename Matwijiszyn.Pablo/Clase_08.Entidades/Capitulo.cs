e3cca8997ff407e91c686de25900318812d19b79
Pablo Matwijiszyn <pmatwiji@gmail.com>
2019-09-17 11:58:14 -0300

    Clase 08

---
+++ Matwijiszyn.Pablo/Clase_08.Entidades/Capitulo.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_08.Entidades
+{
+    public class Capitulo
+    {
+        private int numero;
+        private string titulo;
+        private int paginas;
+
+        private static Random generadorDeNumeros;
+        private static Random generadorDePaginas;
+
+
+
+        public int MiNumero
+        {
+            get { return this.numero; }
+
+        }
+
+
+        public string MiTitulo
+        {
+            get { return this.titulo; }
+        }
+
+        public int MisPaginas
+        {
+            get { return this.paginas; }
+        }
+
+
+        static Capitulo()
+        {
+            Capitulo.generadorDeNumeros = new Random();
+            Capitulo.generadorDePaginas = new Random();
+        }
+
+        private Capitulo(int numero, string titulo, int paginas)
+        {
+            this.numero = numero;
+            this.titulo = titulo;
+            this.paginas = paginas;
+        }
+
+        public static implicit operator Capitulo(string capitulo)
+        {
+            int paginas = Capitulo.generadorDePaginas.Next(15, 233);
+            int numeros = Capitulo.generadorDeNumeros.Next(1, 65);
+
+            return new Capitulo(numeros, capitulo, paginas);
+        }
+
+        public static bool operator ==(Capitulo capituloUno, Capitulo capituloDos)
+        {
+            if(capituloUno.numero == capituloDos.numero && capituloUno.titulo == capituloDos.titulo)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator !=(Capitulo capituloUno, Capitulo capituloDos)
+        {
+            return !(capituloUno == capituloDos);
+        }
+
+
+
+    }
+}
---
+++ Matwijiszyn.Pablo/Clase_08.Entidades/Libro.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_08.Entidades
+{
+    public class Libro
+    {
+        private string Titulo;
+
+        public string MiTitulo
+        {
+            get { return Titulo; }
+        }
+
+        private string Autor;
+
+        public string MiAutor
+        {
+            get { return Autor; }
+        }
+
+        private List<Capitulo> ListCapitulos;// = new List<Capitulo>();
+
+        public Libro(string titulo,string autor)
+        {
+            this.Titulo = titulo;
+            this.Autor = autor;
+            this.ListCapitulos = new List<Capitulo>();
+        }
+
+
+
+
+        //Prop s/l CantidadPaginas : int sumar paginas de capitulos
+
+        
+
+        public int CantidadDePaginas
+        {
+            get
+            {
+                int total = 0;
+                foreach (Capitulo capitulos in ListCapitulos)
+                {
+                    total += capitulos.MisPaginas;
+                }
+
+                return total;
+            }
+        }
+
+        public int CantidadDeCapitulos
+        {
+            get
+            {
+                int total = 0;
+                foreach (Capitulo capitulos in ListCapitulos)
+                {
+                    total ++;
+                }
+
+                return total;
+            }
+        }
+        //Indexador de libro
+
+        public Capitulo this[int i]
+        {
+            get
+            {
+                if(this.ListCapitulos[i] != null)
+                {
+                    return this.ListCapitulos[i];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            set { }
+        }
+
+        //get si indice excede rango, retorna null
+        //get si es valido, retorno capitulo en ese indice
+
+        //set si indice supera rango+1, se agrega como nuevo capitulo
+        //indice inferior a 0 no se puede xxxx
+        //indice entre 0 y max.indice, reemplazo en ese indice
+    }
+}
